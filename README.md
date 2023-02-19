6b02c0a7a977f04fbe20bac2e94d8aa40de6783e
DavidDGamble <daviddgamble@gmail.com>
2023-02-18 16:39:41 -0800

    Add all code to project.

---
+++ README.md
@@ -0,0 +1,11 @@
+## Notes!
+
+#### Create .env file in root repository 
+###### find the follwing info on firebase site under project settings
+_Note: It is very important that every environment variable in your application is prefaced by REACT_APP. Otherwise, the environment variable won't work. This is a safeguard put in place by create-react-app to ensure that sensitive environment variables aren't accidentally exposed in our applications._
+- REACT_APP_FIREBASE_API_KEY
+- REACT_APP_FIREBASE_AUTH_DOMAIN 
+- REACT_APP_FIREBASE_PROJECT_ID
+- REACT_APP_FIREBASE_STORAGE_BUCKET 
+- REACT_APP_FIREBASE_MESSAGING_SENDER_ID
+- REACT_APP_FIREBASE_APP_ID
---
+++ src/components/TicketControl.js
@@ -0,0 +1,112 @@
+import React, { useEffect, useState } from 'react';
+import NewTicketForm from './NewTicketForm';
+import TicketList from './TicketList';
+import EditTicketForm from './EditTicketForm';
+import TicketDetail from './TicketDetail';
+import db from './../firebase';
+import { collection, addDoc, doc, updateDoc, deleteDoc, onSnapshot } from 'firebase/firestore';
+
+function TicketControl() {
+  const [formVisibleOnPage, setFormVisibleOnPage] = useState(false);
+  const [mainTicketList, setMainTicketList] = useState([]);
+  const [selectedTicket, setSelectedTicket] = useState(null);
+  const [editing, setEditing] = useState(false);
+
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const unSubscribe = onSnapshot(
+      collection(db, 'tickets'),
+      (collectionSnapshot) => {
+        const tickets = collectionSnapshot.docs.map((doc) => {
+          return {
+            // ... doc.data(), could replace names, location, and issue assignment
+            names: doc.data().names, 
+            location: doc.data().location, 
+            issue: doc.data().issue, 
+            id: doc.id
+          };
+        });
+        setMainTicketList(tickets);
+      },
+      (error) => {
+        setError(error.message);
+      }
+    );
+
+    return () => unSubscribe();
+  }, []);
+  
+  const handleClick = () => {
+    if (selectedTicket != null) {
+      setFormVisibleOnPage(false);
+      setSelectedTicket(null);
+      setEditing(false);
+    } else {
+      setFormVisibleOnPage(!formVisibleOnPage);
+    };
+  };
+
+  const handleDeletingTicket = async (id) => {
+    await deleteDoc(doc(db, 'tickets', id));
+    setSelectedTicket(null);
+  };
+
+  const handleEditClick = () => {
+    setEditing(true);
+  };
+
+  const handleEditingTicketInList = async (ticketToEdit) => {
+    const ticketRef = doc(db, 'tickets', ticketToEdit.id);
+    await updateDoc(ticketRef, ticketToEdit);
+    setEditing(false);
+    setSelectedTicket(null);
+  };
+
+  const handleAddingNewTicketToList = async (newTicket) => {
+    await addDoc(collection(db, 'tickets'), newTicket);
+    setFormVisibleOnPage(false);
+  };
+
+  const handleChangingSelectedTicket = (id) => {
+    const selection = mainTicketList.filter(ticket => ticket.id === id)[0];
+    setSelectedTicket(selection);
+  };
+
+  let currentlyVisibleState = null;
+  let buttonText = null; 
+
+  if (error) {
+    currentlyVisibleState = <p>There was an error: {error}</p>
+  } else if (editing ) {      
+    currentlyVisibleState = <EditTicketForm 
+      ticket = {selectedTicket} 
+      onEditTicket = {handleEditingTicketInList} />
+    buttonText = "Return to Ticket List";
+  } else if (selectedTicket != null) {
+    currentlyVisibleState = <TicketDetail 
+    ticket={selectedTicket} 
+    onClickingDelete={handleDeletingTicket}
+    onClickingEdit = {handleEditClick} />
+    buttonText = "Return to Ticket List";
+  } else if (formVisibleOnPage) {
+    currentlyVisibleState = <NewTicketForm 
+      onNewTicketCreation={handleAddingNewTicketToList}/>;
+    buttonText = "Return to Ticket List"; 
+  } else {
+    currentlyVisibleState = <TicketList 
+      onTicketSelection={handleChangingSelectedTicket} 
+      ticketList={mainTicketList} />;
+    buttonText = "Add Ticket"; 
+  };
+
+  return (
+    <React.Fragment>
+      {currentlyVisibleState}
+      {error ? null : <button onClick={handleClick}>{buttonText}</button>} 
+    </React.Fragment>
+  );
+};
+
+export default TicketControl;
+
